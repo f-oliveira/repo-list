6016f333fb78199eeb7aed2d7cc7cdfc9cc6aefd
Felipe N. Oliveira <felipe.noliveira19@gmail.com>
2023-05-16 19:50:23 -0300

    feat: create styles.

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,7 +1,38 @@
-import React from "react";
+import React, { useState } from "react";
+import {FaGithub, FaPlus} from 'react-icons/fa';
+import {Container, Form, SubmitButton} from './styles';
 
 export default function Main() {
+  const [newRepository, setNewRepository] =  useState('');
+
+  function handleInputChange(e) {
+    setNewRepository(e.target.value);
+  }
+
+  function handleSubmit(e) {
+    e.preventDefault();
+  }
+
   return(
-    <h1>Main</h1>
+    <Container>
+
+      <h1>  
+        <FaGithub size={25}/>
+        Meus repositórios
+      </h1>
+
+      <Form onSubmit={()=>{}}>
+        <input 
+          type="text" 
+          placeholder="Adicionar repositórios"
+          value={newRepository}
+          onChange={handleInputChange}
+        />
+
+        <SubmitButton>
+          <FaPlus color="#FFF" size={14} />
+        </SubmitButton>
+      </Form>
+    </Container>
   )
 }
