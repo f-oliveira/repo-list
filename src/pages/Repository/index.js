a682897075ea9cc09ecb9d7b3db7d69924aa59af
Felipe N. Oliveira <felipe.noliveira19@gmail.com>
2023-05-19 10:17:06 -0300

    feat: loading issues.

--- src/pages/Repository/index.js
+++ src/pages/Repository/index.js
@@ -1,7 +1,42 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
+import { Container } from "./styles";
+
+import api from '../../services/api';
 
 export default function Repository() {
+
+  const [repositorio, setRepositorio] = useState({});
+  const [issues, setIssues] = useState([]);
+  const [loading, setLoading] = useState(true);
+  
+  useEffect(()=> {
+    
+    async function load(){
+      const nomeRepo = decodeURIComponent(match.params.repositorio);
+
+      const [repositorioData, issuesData] = await Promise.all([
+        api.get(`/repos/${nomeRepo}`),
+        api.get(`/repos/${nomeRepo}/issues`, {
+          params:{
+            state: 'open',
+            per_page: 5
+          }
+        })
+      ]);
+
+      setRepositorio(repositorioData.data);
+      setIssues(issuesData.data);
+      setLoading(false);
+
+    }
+
+    load();
+
+  }, [match.params.repositorio]);
+  
   return(
-    <h1>Repositorio</h1>
+    <Container>
+      
+    </Container>
   )
 }
